38b1296227d26059ecf42ff6ba939187112cceb5
crushedace <93542325+crushedace@users.noreply.github.com>
2023-07-05 05:45:44 +0000

    check this out ladss

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+"use client";
+import NextAuth from 'next-auth/next';
+import {getProviders, signIn}from 'next-auth/react';
+import { type } from 'os';
+import {useState, useEffect}from 'react';
+
+type Provider = {
+  id: string ;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams: Record<string,string>
+  |null;
+}
+type Providers = Record<string,Provider>;
 
 const AuthProviders = () => {
+  const[providers,setProviders]= useState<Providers|null>(null);
+  
+
   return (
     <div>AuthProviders</div>
   )
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ type ColumnProps ={
 }
 const FooterColumn =({title,links}: ColumnProps)=>( 
   <div className="footer_column">
-    <h4 className="font-seminold"> {title}</h4>
+    <h4 className="font-semibold"> {title}</h4>
     <ul className = "flex flex-col gap-2 font-normal">
       {links.map((link)=> <Link href="/" key={link}>{link}</Link>)}
     </ul>
@@ -19,7 +19,7 @@ const FooterColumn =({title,links}: ColumnProps)=>(
 const Footer = () => {
   return (
     <footer className="flexStart footer">
-      <div className="flerx flex-col gap-12 w-full">
+      <div className="flex flex-col gap-12 w-full">
         <div className= "flex items-start flex-col">
           <Image
           src="/logo-purple.svg"
@@ -53,8 +53,8 @@ const Footer = () => {
           <FooterColumn title={footerLinks[5].title}
           links={footerLinks[5].links}/>
           </div>
-          <FooterColumn title={footerLinks[3].title}
-          links={footerLinks[3].links}/>
+          <FooterColumn title={footerLinks[6].title}
+          links={footerLinks[6].links}/>
         </div>
       </div>
       <div className = "flexBetween footer_copyright">
